c1b7ae5fc21d0b4d86416ddeadb32722615c6101
Wojciech Mucha <w.mucha@weegree.com>
2019-05-12 19:48:30 +0200

    changed url

--- configs/urls.ts
+++ configs/urls.ts
@@ -1,6 +1,6 @@
 
 export class Urls {
-  public static baseUrl: string = process.env.BASE_URL || 'localhost:3000';
+  public static baseUrl: string = process.env.BASE_URL || 'https://angularjs.org/';
   
   public static homePage: string = `${Urls.baseUrl}/home`;
   public static loginPage: string = `${Urls.baseUrl}/`;
